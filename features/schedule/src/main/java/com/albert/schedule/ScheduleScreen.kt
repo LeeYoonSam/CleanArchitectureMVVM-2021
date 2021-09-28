17ee26ec85793562d809dd3a26c9042e3a1d24b6
YS <yoonsam86@gmail.com>
2021-09-28 18:39:48 +0900

    Track 별 Pager 추가

--- features/schedule/src/main/java/com/albert/schedule/ScheduleScreen.kt
+++ features/schedule/src/main/java/com/albert/schedule/ScheduleScreen.kt
@@ -1,19 +1,76 @@
 package com.albert.schedule
 
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Divider
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.albert.shared.model.Session
 import com.albert.ui_core_compose.util.toColor
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.HorizontalPagerIndicator
+import com.google.accompanist.pager.rememberPagerState
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun ScheduleScreen(
     sessions: List<Session>,
     onSessionClick: (Session) -> Unit = { }
+) {
+    val groupingSession = sessions.groupBy {
+        it.room
+    }
+
+    val rooms = groupingSession.keys.toList()
+    val pagerState = rememberPagerState(pageCount = rooms.size)
+    
+    Column(Modifier.fillMaxSize()) {
+        Row(
+            modifier = Modifier.padding(24.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = rooms[pagerState.currentPage],
+                color = "#2F2E32".toColor,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.weight(1f)
+            )
+            HorizontalPagerIndicator(
+                pagerState = pagerState,
+                activeColor = "#4cc786".toColor,
+                inactiveColor = "#4cc786".toColor.copy(alpha = 0.3f),
+                indicatorWidth = 10.dp
+            )
+        }
+
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.Top
+        ) { page ->
+            Schedules(
+                sessions = groupingSession[rooms.elementAt(page)].orEmpty(),
+                onSessionClick = onSessionClick
+            )
+        }
+    }
+}
+
+@Composable
+fun Schedules(
+    modifier: Modifier = Modifier,
+    sessions: List<Session>,
+    onSessionClick: (Session) -> Unit = {}
 ) {
     LazyColumn {
         items(items = sessions) { session ->
