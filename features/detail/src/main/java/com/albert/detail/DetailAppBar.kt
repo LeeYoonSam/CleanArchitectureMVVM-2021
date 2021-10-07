506157c23e56749aade757c1e7e0e2954f91965a
YS <yoonsam86@gmail.com>
2021-10-07 18:16:26 +0900

    AppBar 공통 사용하도록 처리

--- features/detail/src/main/java/com/albert/detail/DetailAppBar.kt
+++ features/detail/src/main/java/com/albert/detail/DetailAppBar.kt
@@ -1,16 +1,13 @@
 package com.albert.detail
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.sp
@@ -19,26 +16,30 @@ import com.albert.core_ui_compose.util.toColor
 @Composable
 fun DetailAppBar(
     title: String,
+    modifier: Modifier = Modifier,
     onNavigationClick: () -> Unit = {},
     onSharedClick: () -> Unit = {}
 ) {
-    TopAppBar(backgroundColor = Color.White) {
-        Row {
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                fontSize = 18.sp,
+                color = "#2F2E32".toColor()
+            )
+        },
+        navigationIcon = {
             IconButton(onClick = onNavigationClick) {
                 Icon(
                     imageVector = Icons.Filled.ArrowBack,
                     contentDescription = null
                 )
             }
-            
-            Text(
-                text = title,
-                fontSize = 18.sp,
-                color = "#2F2E32".toColor(),
-                modifier = Modifier
-                    .weight(1f)
-                    .align(Alignment.CenterVertically)
-            )
-        }
-    }
+        },
+        actions = {
+
+        },
+        modifier = modifier.fillMaxWidth(),
+        backgroundColor = Color.White
+    )
 }
