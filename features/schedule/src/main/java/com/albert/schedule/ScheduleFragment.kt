1209ed1052ea668f0fe0c8335b8adbfed28fb857
YS <yoonsam86@gmail.com>
2021-10-08 15:40:14 +0900

    중복 에러 코드 개선

--- features/schedule/src/main/java/com/albert/schedule/ScheduleFragment.kt
+++ features/schedule/src/main/java/com/albert/schedule/ScheduleFragment.kt
@@ -5,8 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
@@ -38,6 +42,8 @@ class ScheduleFragment : Fragment() {
     ): View {
         return setThemeContent {
             val result by viewModel.sessions.observeAsState(UiState.loading())
+            val scrollState = rememberScrollState()
+
             LoadingContent(
                 loading = result.initialLoad,
                 loadingContent = { FullScreenLoading() },
@@ -46,19 +52,21 @@ class ScheduleFragment : Fragment() {
                     Column(
                         modifier = Modifier
                             .fillMaxSize()
+                            .verticalScroll(scrollState)
                             .padding(24.dp),
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
                         Icon(
-                            painter = painterResource(id = R.drawable.ic_error_black_24),
+                            imageVector = Icons.Filled.Warning,
                             modifier = Modifier.size(64.dp),
                             contentDescription = null
                         )
                         Spacer(modifier = Modifier.height(10.dp))
-                        Text(text = result.exception?.localizedMessage.orEmpty())
-                        Timber.e(result.exception)
-
+                        Text(
+                            text = result.exception?.localizedMessage.orEmpty(),
+                            modifier = Modifier.fillMaxWidth()
+                        )
                     }
                 }
             ) {
